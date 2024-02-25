d6a5a31adecaa9f5fe9e80ef11f71f2f01fc2ef3
MAYCON PACHECO <151109977+mayconpachecooficial@users.noreply.github.com>
2024-02-24 23:40:43 -0400

    atualização

--- janela.py
+++ janela.py
@@ -1,18 +1,18 @@
-# import tkinter 
+import tkinter 
 
-# janela = tkinter.Tk()
-# janela.geometry('500x300')
+janela = tkinter.Tk()
+janela.geometry('500x300')
 
-# def clique():
-#     print('Fazer Login')
+def clique():
+    print('Fazer Login')
 
-# texto = tkinter.Label(janela, text='Fazer Login')
-# texto.pack(padx=10, pady=10)
+texto = tkinter.Label(janela, text='Fazer Login')
+texto.pack(padx=10, pady=10)
 
-# botao = tkinter.Button(janela, text='Login', command=clique)
-# botao.pack(padx=10, pady=10)
+botao = tkinter.Button(janela, text='Login', command=clique)
+botao.pack(padx=10, pady=10)
 
-# janela.mainloop()
+janela.mainloop()
 
 import customtkinter
 
